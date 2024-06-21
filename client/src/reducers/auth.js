78ff8784e165cf8dbced0de59f1d5173147bd585
yaalu18 <yaalu18@gmail.com>
2024-06-21 11:25:11 +0530

    why profile is not showing

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,7 +5,7 @@ const authReducer = (state = { data: null },action) => {
     switch (action.type) {
       case "AUTH":
         console.log('am i dispatching auth')
-        //console.log("this is the thing",action?.data)
+        console.log("this is the thing",action?.data)
         localStorage.setItem("Profile", JSON.stringify({ ...action?.data }));
         console.log('why its not moving forward')
         return { ...state, data: action?.data };
