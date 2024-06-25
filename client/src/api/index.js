003e538fe7207710d15dff08a00f44666f9859dd
yaalu18 <yaalu18@gmail.com>
2024-06-25 14:05:01 +0530

    question section 2 clientside

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -17,8 +17,8 @@ API.interceptors.request.use((req) => {
 export const logIn = (authData) => API.post("/user/login", authData);
 export const signUp = (authData) => API.post("/user/signup", authData);
 
-//export const postQuestion = (questionData) =>
-  //API.post("/questions/Ask", questionData);
+export const postQuestion = (questionData) =>
+  API.post("/questions/Ask", questionData);
 //export const getAllQuestions = () => API.get("/questions/get");
 //export const deleteQuestion = (id) => API.delete(/questions/delete/${id});
 //export const voteQuestion = (id, value) =>
