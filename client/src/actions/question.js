43e77c3bd113b1ceaf03d4663d08952dc4b529aa
yaalu18 <yaalu18@gmail.com>
2024-06-26 15:50:51 +0530

    found the issue in actions

--- client/src/actions/question.js
+++ client/src/actions/question.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as api from '../api';
-export const askQuestion=(questinData,navigate)=>async (dispatch)=>{
+export const askQuestion=(questionData,navigate)=>async (dispatch)=>{
     try{
         const {data}=await api.postQuestion(questionData)
         dispatch({type:"POST_QUESTION",payload:data})
