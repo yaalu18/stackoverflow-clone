83812002250cac637d8e3b11f010e9bf78b09539
yaalu18 <yaalu18@gmail.com>
2024-07-01 13:26:42 +0530

    final try1

--- client/src/reducers/questions.js
+++ client/src/reducers/questions.js
@@ -3,9 +3,9 @@ const questionsReducer=(state={data:null},action)=>{
         case "POST_QUESTION":
             return {...state}
         case "FETTCH_ALL_QUESTION":
-            //return {...state,data:action.payload}
-            const response = await api.getAllQuestions(); // Fetch questions from MongoDB
-            dispatch({ type: 'FETCH_ALL_QUESTION', payload: response.data }); // Dispatch action with fetched data
+            return {...state,data:action.payload}
+           // const response = await api.getAllQuestions(); // Fetch questions from MongoDB
+            //dispatch({ type: 'FETCH_ALL_QUESTION', payload: response.data }); // Dispatch action with fetched data
         default:
             return state
     }
