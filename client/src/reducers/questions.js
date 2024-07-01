be803f14f2a4f16730bb0484fa11b27fc901c35c
yaalu18 <yaalu18@gmail.com>
2024-07-01 14:10:35 +0530

    final try3

--- client/src/reducers/questions.js
+++ client/src/reducers/questions.js
@@ -1,7 +1,11 @@
 const questionsReducer=(state={data:null},action)=>{
     switch(action.type){
         case "POST_QUESTION":
-            return {...state}
+            //return {...state}
+            return {
+                ...state,
+                data: state.data ? [...state.data, action.payload] : [action.payload]
+            };
         case "FETTCH_ALL_QUESTION":
             return {...state,data:action.payload}
            // const response = await api.getAllQuestions(); // Fetch questions from MongoDB
